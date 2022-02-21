dcb2e8ca5275b148b3490d8ad590a70f6f5ef03e
Yawson-Enoch <enochgybex25@gmail.com>
2022-02-21 16:08:33 +0000

    build base page layout

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,31 @@
-console.log('never stop learning');
+const nav = document.querySelector('.header-nav');
+const navLinks = nav.querySelector('.header-nav__links');
+const navToggler = document.querySelector('.nav-toggler');
+const cancelBtn = navToggler.querySelector('.cancel');
+const hamburgerBtn = navToggler.querySelector('.hamburger');
+
+const toggleDisplay = (elementName, displayValue) => {
+  elementName.style.display = displayValue;
+};
+
+let navLinksHeight = navLinks.getBoundingClientRect().height;
+
+navToggler.addEventListener('click', () => {
+  navLinksHeight = navLinks.getBoundingClientRect().height;
+  cancelBtn.classList.toggle('cancel-active');
+
+  let isActive = cancelBtn.classList.contains('cancel-active');
+
+  if (isActive) {
+    nav.style.height = `${navLinksHeight}px`;
+    toggleDisplay(cancelBtn, 'flex');
+    toggleDisplay(hamburgerBtn, 'none');
+  } else {
+    nav.style.height = '0px';
+    toggleDisplay(hamburgerBtn, 'flex');
+    toggleDisplay(cancelBtn, 'none');
+  }
+});
+
+const footerYear = document.querySelector('.year');
+footerYear.innerText = new Date().getUTCFullYear().toString();
